3fb743f8bcae9d86953c7ac24f56015c869fbfda
Ardiansyah <aardi25@gmail.com>
2022-03-12 09:26:39 +0700

    Add project files.

---
+++ LoadForm/FormMenuUtama.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoadForm
+{
+    public partial class FormMenuUtama : Form
+    {
+        //Ubah menjadi object
+        FormBarang frmBrg;
+        //Buat Procedure Close
+        void FrmBrg_FormClosed(Object sender, FormClosedEventArgs e)
+        {
+            frmBrg = null;
+        }
+        public FormMenuUtama()
+        {
+            InitializeComponent();
+        }
+
+        private void barangToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(frmBrg == null)
+            {
+                frmBrg = new FormBarang();
+                frmBrg.FormClosed += new FormClosedEventHandler(FrmBrg_FormClosed);
+                frmBrg.Show();
+            }
+            else
+            {
+                frmBrg.Activate();
+            }
+        }
+    }
+}
